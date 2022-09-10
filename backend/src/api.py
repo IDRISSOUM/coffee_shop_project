c811ee4613d9d021f1b40738398a84ac7769b088
IDRISSOU Marzouk <islamsunnaestlaveriter@gmail.com>
2022-09-10 12:34:44 +0000

    oooooo

--- backend/src/api.py
+++ backend/src/api.py
@@ -107,6 +107,48 @@ def unprocessable(error):
     error handler should conform to general task above
 '''
 
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+    "success": False,
+    "error": 400,
+    "message": "bad request"
+    }), 400
+    
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+    "success": False,
+    "error": 404,
+    "message": "resource not found"
+    }), 404
+
+@app.errorhandler(405)
+def not_found(error):
+    return jsonify({
+    "success": False,
+    "error": 405,
+    "message": "method not allowed"
+    }), 405
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+    "success": False,
+    "error": 422,
+    "message": "unprocessable"
+    }), 422
+
+@app.errorhandler(500)
+def  bad_request(error):
+    return jsonify({
+    "success": False,
+    "error": 500,
+    "message": "internal error server"
+    }), 500
+
+
+
 
 '''
 @TODO implement error handler for AuthError
